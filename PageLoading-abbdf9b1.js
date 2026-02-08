02833d89948978cfa33f4d3ea3c780e82b281c5b
ourschoolbd <bdourschool@gmail.com>
2026-02-08 13:04:13 +0600

    Create PageLoading-abbdf9b1.js

---
+++ PageLoading-abbdf9b1.js
@@ -0,0 +1,14 @@
+import {j as e} from "./index-4f7d73dc.js";
+const s = () => e.jsx("div", {
+    className: "h-[95vh] min-w-screen flex justify-center items-center bg-transparent",
+    children: e.jsx("div", {
+        className: "w-1/2 bg-gray-200 h-1 rounded-full overflow-hidden relative mb-2",
+        children: e.jsx("div", {
+            className: "absolute top-0 left-0 h-1 bg-gray-500 animate-indeterminate",
+            style: {
+                width: "50%"
+            }
+        })
+    })
+});
+export {s as P};
