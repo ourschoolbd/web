6eb1b6f37228288965389d6ecea729a6a914f626
ourschoolbd <bdourschool@gmail.com>
2026-02-08 13:07:06 +0600

    Create ContactButtons-ddfdceef.js

---
+++ ContactButtons-ddfdceef.js
@@ -0,0 +1,52 @@
+import {j as e, aw as s, ax as r, ay as a, az as n} from "./index-4f7d73dc.js";
+const l = () => e.jsxs("div", {
+    className: "flex gap-2 flex-col items-center justify-center mt-5",
+    children: [e.jsx("a", {
+        href: "tel:01711476706",
+        children: e.jsxs("div", {
+            className: "flex gap-2 items-center justify-center bg-gray-200 hover:bg-gray-100 w-60 p-2 rounded-full",
+            children: [e.jsx(s, {}), e.jsx("span", {
+                children: "+880 1711-476706"
+            })]
+        })
+    }), e.jsx("a", {
+        href: "https://wa.me/message/GBAI3JOCAGMLB1",
+        target: "_blank",
+        rel: "noopener noreferrer",
+        children: e.jsxs("div", {
+            className: "flex gap-2 items-center justify-center bg-[#25d366] hover:bg-green-400 text-white w-60 p-2 rounded-full",
+            children: [e.jsx(r, {}), e.jsx("span", {
+                children: "Open WhatsApp"
+            })]
+        })
+    }), e.jsx("a", {
+        href: "https://m.me/eproshnobank",
+        target: "_blank",
+        rel: "noopener noreferrer",
+        children: e.jsxs("div", {
+            className: "flex gap-2 items-center justify-center bg-blue-600 hover:bg-blue-500 text-white w-60 p-2 rounded-full",
+            children: [e.jsx(a, {}), e.jsx("span", {
+                children: "Open Messenger"
+            })]
+        })
+    }), e.jsxs("div", {
+        className: "flex items-center justify-center mt-3 w-full",
+        children: [e.jsx("div", {
+            className: "border-t border-gray-300 flex-grow mr-2"
+        }), e.jsx("span", {
+            className: "text-gray-500",
+            children: "OR"
+        }), e.jsx("div", {
+            className: "border-t border-gray-300 flex-grow ml-2"
+        })]
+    }), e.jsx("a", {
+        href: "mailto:eproshnobank@gmail.com",
+        children: e.jsxs("div", {
+            className: "flex gap-2 items-center text-gray-600 hover:text-gray-500",
+            children: [e.jsx(n, {}), e.jsx("span", {
+                children: "Mail: hello@eproshnobank.com"
+            })]
+        })
+    })]
+});
+export {l as C};
