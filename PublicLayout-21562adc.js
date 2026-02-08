d2dd2b10b3a231b245dee2d7370d08178981c256
ourschoolbd <bdourschool@gmail.com>
2026-02-08 13:03:23 +0600

    Create PublicLayout-21562adc.js

---
+++ PublicLayout-21562adc.js
@@ -0,0 +1,6 @@
+import {j as s, p as t, O as r} from "./index-4f7d73dc.js";
+import {P as a} from "./config-e19b493c.js";
+const i = () => s.jsxs("div", {
+    children: [s.jsx(t, {}), !a, s.jsx(r, {})]
+});
+export {i as default};
